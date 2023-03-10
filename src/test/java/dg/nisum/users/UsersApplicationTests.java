50110d18d3cc53e43dee04c8295f8f33faad52dd
Daycith Gonzalez <daycith1@gmail.com>
2023-03-09 20:29:15 -0500

    first commit

---
+++ src/test/java/dg/nisum/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package dg.nisum.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
